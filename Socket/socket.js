e899c13ed5c4d879455db9f4db32c5f5b0d938a8
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-09-20 21:56:29 +0530

    Added The Original Link

--- Socket/socket.js
+++ Socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app)
 const io = new Server(server,{
     cors:{
-        origin:['http://localhost:3000'],
+        origin:['https://whatsgram-jm9c.onrender.com/'],
         methods:["GET","POST"]
     }
 });
